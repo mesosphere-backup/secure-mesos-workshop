7b53c0f5b8e622055a513437b03ba33eea85c17f
Adam B <adam@mesosphere.io>
2017-09-13 13:38:50 -0700

    Nit: Comment typo in generate_ssl_key_cert.sh.

--- scripts/generate_ssl_key_cert.sh
+++ scripts/generate_ssl_key_cert.sh
@@ -7,5 +7,5 @@ mkdir -p ssl
 openssl genrsa -f4 -out ssl/key.pem 4096
 
 
-# Generate a certificte.
+# Generate a certificate.
 openssl req -new -x509 -days 365 -key ssl/key.pem -out ssl/cert.pem -subj "/C=US"
